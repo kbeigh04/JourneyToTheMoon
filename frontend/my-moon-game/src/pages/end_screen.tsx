6172a732bc16020ce3a6a84dd31e9ecf1996d71c
Kayla Beigh <kaylabeigh@Kaylas-MacBook-Pro-9.local>
2025-11-24 16:34:17 -0500

    initial scaffold

---
+++ frontend/my-moon-game/src/pages/end_screen.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const End: React.FC = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "2rem",
+      }}
+    >
+      🚀 End Screen
+    </div>
+  );
+};
---
+++ frontend/my-moon-game/src/pages/start_screen.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Start: React.FC = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "2rem",
+      }}
+    >
+      🚀 Start Screen
+    </div>
+  );
+};
+
+export default Start;
